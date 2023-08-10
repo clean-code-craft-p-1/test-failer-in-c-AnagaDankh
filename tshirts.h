b9ccdfa870869ae9e8d252766a6315ad399b8773
AnagaDankh <122591543+AnagaDankh@users.noreply.github.com>
2023-08-10 14:31:00 +0530

    Update tshirts.h

--- tshirts.h
+++ tshirts.h
@@ -3,14 +3,4 @@
 
 void tshirtssize(void);
 
-char size(int cms) {
-    char sizeName = '\0';
-    if(cms < 38) {
-        sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
-        sizeName = 'M';
-    } else if(cms > 42) {
-        sizeName = 'L';
-    }
-    return sizeName;
-}
+
