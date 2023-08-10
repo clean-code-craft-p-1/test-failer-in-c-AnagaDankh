456b73af355078520576367df0240c55b3f19fcd
AnagaDankh <122591543+AnagaDankh@users.noreply.github.com>
2023-08-10 14:30:53 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -4,10 +4,24 @@ int main()
 {
     tshirtssize();
 }
+
+char size(int cms) {
+    char sizeName = '\0';
+    if(cms < 38) {
+        sizeName = 'S';
+    } else if(cms > 38 && cms < 42) {
+        sizeName = 'M';
+    } else if(cms > 42) {
+        sizeName = 'L';
+    }
+    return sizeName;
+}
 void tshirtssize()
 {
     assert(size(37) == 'S');
+    assert(size(38) == 'S');
     assert(size(40) == 'M');
+    assert(size(42) == 'M');
     assert(size(43) == 'L');
     printf("All is well (maybe!)\n");
 }
